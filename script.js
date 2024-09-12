32ee332683b59f574ea789c85efecd5eb8300684
Anthony Lansing <lansinganthony85@gmail.com>
2024-09-12 17:29:55 -0400

    Started the layout of the menu, navigation, and first two sections. Working on javascript to properly space elements based on the viewport. Will next work on animations and properly highlighting of items in the menu.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get the root element to have access to variables
+const root = document.querySelector(":root");
+
+// Get all the sections of the DOM
+const main_section = document.getElementById("anthony_section");
+const about_section = document.getElementById("about_section");
+
+// Get the size of the viewport
+const window_height = window.innerHeight;
+const window_width = window.innerWidth;
+
+// set starting margins for the main content and the different sections
