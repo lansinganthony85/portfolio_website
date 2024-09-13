0d851754039772306ef006d7087afcfbe2e03d0f
Anthony Lansing <lansinganthony85@gmail.com>
2024-09-13 11:46:02 -0400

    Got the first two sections to center correctly when full screen. Navigation menu correctly centers on the section.

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 const root = document.querySelector(":root");
 
 // Get all the sections of the DOM
+const main_element = document.querySelector("main");
 const main_section = document.getElementById("anthony_section");
 const about_section = document.getElementById("about_section");
 
-// Get the size of the viewport
-const window_height = window.innerHeight;
-const window_width = window.innerWidth;
-
-// set starting margins for the main content and the different sections
